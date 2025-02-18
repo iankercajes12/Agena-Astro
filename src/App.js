0c2315e30e43c59d755e0dc1e0f00c4ced23b36b
iankercajes12 <40867950+iankercajes12@users.noreply.github.com>
2025-02-18 19:56:48 +0800

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ import UgcGallery from "./components/UgcGallery";
 function App() {
   //Instance ID
   let InstanceId_Widget = "660773";
-  let InstanceId_QA = "";
+  let InstanceId_QA = "1040855";
   let InstanceId_SEO = "";
   let InstanceId_Carousel = "";
   let InstanceId_StarRating = "1020700";
